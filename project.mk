333bb554afa7513d7144c4fa6e74dd74a33463d6
Jeffrey A. Clark (Alex) <aclark@aclark.net>
2024-05-04 11:19:23 -0400

    Add error html and website config

--- project.mk
+++ project.mk
@@ -6,8 +6,10 @@ create-bucket:
 
 list-buckets:
 	aws s3 ls | grep test
+	aws s3 website s3://$(BUCKET_NAME) --index-document index.html --error-document error.html
 
 deploy:
 	aws s3 cp index.html s3://$(BUCKET_NAME)/index.html
+	aws s3 cp error.html s3://$(BUCKET_NAME)/error.html
 
 test-s3-deploy: create-bucket list-buckets deploy
