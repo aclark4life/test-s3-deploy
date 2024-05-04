1548bf1434c6fb1edd9a5b598856752e36f8df90
Jeffrey A. Clark (Alex) <aclark@aclark.net>
2024-05-04 12:13:13 -0400

    Update targets

--- project.mk
+++ project.mk
@@ -4,12 +4,9 @@ BUCKET_NAME = test-s3-deploy-$(GIT_REV)
 create-bucket:
 	aws s3api create-bucket --bucket $(BUCKET_NAME) | cat
 
-list-buckets:
-	aws s3 ls | grep test
-	aws s3 website s3://$(BUCKET_NAME) --index-document index.html --error-document error.html
-
-deploy:
+create-website:
 	aws s3 cp index.html s3://$(BUCKET_NAME)/index.html
 	aws s3 cp error.html s3://$(BUCKET_NAME)/error.html
+	aws s3 website s3://$(BUCKET_NAME) --index-document index.html --error-document error.html
 
-test-s3-website: create-bucket deploy
+test-s3-website: create-bucket html-index html-error create-website
